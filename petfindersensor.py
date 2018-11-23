78dc1cacf02ef4f6096ac4798e3a5a43a6feb075
Roger Hinson <rogerhinson@gmail.com>
2018-11-23 15:11:10 -0800

    Added origin of pet

--- petfindersensor.py
+++ petfindersensor.py
@@ -3,11 +3,11 @@ A Sensor to retrieve a list of animals available for adoption at
 the El Cajon Animal Shelter using the PetFinder API
 """
 
-__version__ = "1.2"
+__version__ = "1.1"
 __author__ = "Roger Hinson"
 __email__ = "roger.hinson@gmail.com"
 
-from sensor import Sensor
+from sensor import SensorX
 import json
 import os
 import time
@@ -17,38 +17,35 @@ import errno
 import re
 from datetime import datetime
 
+__LOG_DIRECTORY = 'petfinderlog'
+__LOG_FILENAME = 'petfinder.log'
 
-class PetFinderSensor(Sensor):
+# Create the logs directory if it doesn't already exist
+try:
+    os.makedirs(os.path.join(os.path.dirname(__file__), __LOG_DIRECTORY))
+except OSError as e:
+    if e.errno != errno.EEXIST:
+        print("Error is: " + str(e))
 
-    __LOG_DIRECTORY = 'petfinderlog'
-    __LOG_FILENAME = 'petfinder.log'
-    __CONFIG_FILE = 'petfindersensor.json'
-    __SAVED_RECORDS = 'petfinder_saved_records.json'
+# Create standard logging entries
+logging.basicConfig(
+    level=logging.INFO,
+    filename=os.path.join(os.path.dirname(__file__), __LOG_DIRECTORY, __LOG_FILENAME),
+    filemode='a',
+    format='%(asctime)s - %(lineno)d - %(levelname)s - %(message)s')
 
-    # Create the logs directory if it doesn't already exist
-    try:
-        os.makedirs(os.path.join(os.path.dirname(__file__), __LOG_DIRECTORY))
-    except OSError as e:
-        if e.errno != errno.EEXIST:
-            print("Error is: " + str(e))
 
-    # Create standard logging entries
-    logging.basicConfig(
-        level=logging.INFO,
-        filename=os.path.join(os.path.dirname(__file__), __LOG_DIRECTORY, __LOG_FILENAME),
-        filemode='a',
-        format='%(asctime)s - %(lineno)d - %(levelname)s - %(message)s')
+class PetFinderSensor(SensorX):
 
     logging.info("\n")  # Add a new line in log to start run
 
     def __init__(self):
         """ Create new PetFinderSensor object and read sensor settings from config file """
         try:
-            with open(os.path.join(os.path.dirname(__file__)) + "/" + PetFinderSensor.__CONFIG_FILE) as json_text:
-                self.d = json.load(json_text)
-            logging.info("This sensor just woke up .. ready to call " + self.d['service_url'])
+            super().__init__(os.path.join(os.path.dirname(__file__), self.__class__.__name__))
+            logging.info("This sensor just woke up .. ready to call " + self.__class__.__name__)
         except (Exception, OSError, ValueError) as e:
-            logging.critical("Not able to read __CONFIG_FILE " + PetFinderSensor.__CONFIG_FILE)
+            logging.critical("Not able to read CONFIG_FILE " + self.__class__.__name__ + '.json')
             logging.critical("Error was: " + str(e))
             exit()
 
@@ -62,7 +59,7 @@ class PetFinderSensor(Sensor):
         if self._request_allowed():
             has_update = self.get_all()
             for update_pet in has_update:
-                if datetime.strptime(update_pet['k'], "%Y-%m-%d"'T'"%H:%M:%S"'Z') > datetime.fromtimestamp(self.d['last_has_update']):
+                if datetime.strptime(update_pet['k'], "%Y-%m-%d"'T'"%H:%M:%S"'Z') > datetime.fromtimestamp(self.props['last_has_update']):
                     update_available = 1
                     logging.info("Updates are available")
                     return update_available
@@ -79,17 +76,22 @@ class PetFinderSensor(Sensor):
         Returns a list of dictionaries for each updated pet
         """
         newContent = []
-        time.sleep(11)  # Since the has_updates ran a get_all it updated the last_get_all time
+        time.sleep(11)  # Since the has_updates ran a get_all it updated the last_used time
         if self._request_allowed():
+            logging.info("get_content _request_allowed passed")
             content = self.get_all()  # Returns all pets with only the data we're looking for
             for pet in content:
-                if datetime.strptime(pet['k'], "%Y-%m-%d"'T'"%H:%M:%S"'Z') > datetime.fromtimestamp(self.d['last_has_update']):
+                if datetime.strptime(pet['k'], "%Y-%m-%d"'T'"%H:%M:%S"'Z') > datetime.fromtimestamp(self.props['last_has_update']):
                     newContent.append(pet)
-            self.d['last_has_update'] = int(time.time())
-            self._save_settings()  # Saves that a request has been made to the service
-            logging.info("Saved last_has_update from get_content")
+            self.props['last_has_update'] = int(time.time())
+            try:
+                self._save_settings()  # Saves that a request has been made to the service
+                logging.info("Saved last_has_update from get_content")
+            except (Exception, OSError, ValueError) as e:
+                logging.warning("Unable to save JSON settings : " + str(e))
+
         else:
-            logging.info("get_content did not allow _request_allowed")
+            logging.info("get_content _request_allowed not passed")
         return newContent
 
     def get_all(self):
@@ -101,40 +103,37 @@ class PetFinderSensor(Sensor):
         Returns a list of dictionaries for all pets
         """
         if self._request_allowed():
-            url = self.d['service_url']+"%s?key=%s&count=%d&id=%s&format=%s"
+            url = self.props['service_url']+"%s?key=%s&count=%d&id=%s&format=%s"
             try:
-                response = requests.get(url % (self.d['service_method'], self.d['key'], self.d['return_count'], self.d['shelter_id'], self.d['format']))
+                response = requests.get(url % (self.props['service_method'], self.props['key'], self.props['return_count'], self.props['shelter_id'], self.props['format']))
             except (requests.ConnectionError, requests.ConnectTimeout) as e:
                 logging.warning("HTTP Request failed for: " + str(e))
-            self.d['last_get_all'] = int(time.time())
-            self._save_settings()  # Saves that a request has been made to the service
-            logging.info("Settings saved in get_all")
+            self.props['last_used'] = int(time.time())
+            try:
+                self._save_settings()  # Saves that a request has been made to the service
+                logging.info("Settings saved in get_all")
+            except (Exception, OSError, ValueError) as e:
+                logging.warning("Unable to save JSON settings : " + str(e))
             try:
                 # URL should return JSON, if not, something went wrong
                 response_json = json.loads(response.text)
-                logging.info("The response from %s was JSON" % (self.d['service_url']))
+                logging.info("The response from %s was JSON" % (self.props['service_url']))
             except (Exception, OSError, ValueError) as e:
                 # The data we received from the response was not in JSON
                 # Something went wrong, so load the saved data
-                logging.warning("The response from %s was not JSON" % (self.d['service_url']))
-                response = self._read_saved_data()
+                logging.warning("The response from %s was not JSON" % (self.props['service_url']))
+                response = self._read_buffer()
                 return self._create_record(json.loads(response))
             if response is not None and response.status_code == 200:
-                try:
-                    with open(os.path.join(os.path.dirname(__file__)) + "/" + PetFinderSensor.__SAVED_RECORDS, 'w') as backup:
-                        backup.write(response.text)
-                        logging.info("Saved response from %s" % (self.d['service_url']))
-                except (Exception, OSError, ValueError) as e:
-                    logging.warning("File response write failed: " + str(e))
-                finally:
-                    return self._create_record(response_json)
+                self._write_buffer(response.text)
+                return self._create_record(response_json)
             else:
                 logging.warning("The response was bad, using saved data")
-                response = self._read_saved_data()
+                response = self._read_buffer()
                 return self._create_record(json.loads(response))
         else:
-            logging.warning("The request was not allowed, using saved data")
-            response = self._read_saved_data()
+            logging.info("The request was not allowed, using saved data")
+            response = self._read_buffer()
             return self._create_record(json.loads(response))
 
     @staticmethod
@@ -170,39 +169,20 @@ class PetFinderSensor(Sensor):
                       "PetFinder ID: " + d['petfinder']['pets']['pet'][pet]['id']['$t']
             # Make all UPPERCASE words BOLD with markdown, word is two or more adjacent UPPERCASE characters
             story = re.sub(r'\b([A-Z][A-Z]+)\b', r'**\1**',
-                           d['petfinder']['pets']['pet'][pet]['description']['$t'])
+                        d['petfinder']['pets']['pet'][pet]['description']['$t'])
+            caption = d['petfinder']['pets']['pet'][pet]['name']['$t']
+            # https://www.petfinder.com/search/pets-for-adoption/?name=stanley&shelter_id[0]=CA141
+            origin = "https://www.petfinder.com/search/pets-for-adoption/?name=" + str.lower(caption) + \
+                        "&shelter_id[0]=" + d['petfinder']['pets']['pet'][pet]['shelterId']['$t']
             record.append({'k': d['petfinder']['pets']['pet'][pet]['lastUpdate']['$t'],
-                           'caption': d['petfinder']['pets']['pet'][pet]['name']['$t'],
+                           'caption': caption,
                            'summary': summary,
                            'story': story,
-                           'img': use_this_photo
+                           'img': use_this_photo,
+                           'origin': origin
                           })
         logging.info("Completed _create_record")
         return record
-
-    def _request_allowed(self):
-        # This use to capture the real allowance of 10,000 times per day
-        # Simplified to new variation for easier grading  :)
-        return not self.d['offline_mode'] and (int(time.time()) - self.d['last_get_all']) > self.d['update_frequency']
-
-    def _save_settings(self):
-        # Save JSON settings to config file
-        try:
-            with open(os.path.join(os.path.dirname(__file__), PetFinderSensor.__CONFIG_FILE), 'w') as outfile:
-                json.dump(self.d, outfile)
-        except (Exception, OSError, ValueError) as e:
-            logging.warning("Unable to save JSON settings : " + str(e))
-
-    def _read_saved_data(self):
-        # Read saved response.text data from saved file in case URL can't be accessed
-        try:
-            with open(PetFinderSensor.__SAVED_RECORDS) as saved_data:
-                response = saved_data.read()
-                logging.info("Read response from saved file")
-            return response
-        except (Exception, OSError, ValueError) as e:
-            logging.critical("Reading saved data error is: " + str(e))
-            return None
 
 
 if __name__ == "__main__":
